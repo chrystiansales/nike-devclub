10aeba749fd2cb5c19b128448094fa90ba8c9fde
Chrystian Sales <chrystianps@gmail.com>
2025-04-04 18:03:38 -0400

    feat: implement color change buttons and update visual elements

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let body = document.querySelector("body");
+let tenis = document.querySelector(".imagem-tenis");
+
+function mudarVisual(cor, imagem) {
+body.style.backgroundColor = cor;
+tenis.src = imagem;
+
+ 
+}
+
