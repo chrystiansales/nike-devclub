a6c7dd898700fcd09f40bb987ee8d2b24de6c236
Chrystian Sales <chrystianps@gmail.com>
2025-04-04 18:11:00 -0400

    feat: update button functionality to change background color and button style

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let body = document.querySelector("body");
 let tenis = document.querySelector(".imagem-tenis");
+let botao = document.querySelector(".botao-carrinho");
 
-function mudarVisual(cor, imagem) {
+function mudarVisual(cor, imagem, carrinho) {
 body.style.backgroundColor = cor;
 tenis.src = imagem;
- 
+botao.style.background = carrinho;
 }
 
