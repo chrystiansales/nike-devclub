b6deb839b4cfa7d8fdb1b5e0914ae96e74b02b06
Chrystian Sales <chrystianps@gmail.com>
2025-04-06 21:42:08 -0400

    fix: remove unused parameter from mudarVisual function to streamline code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,13 @@ let body = document.querySelector("body");
 let tenis = document.querySelector(".imagem-tenis");
 let botao = document.querySelector(".botao-carrinho");
 
-function mudarVisual(cor, imagem, carrinho) {
+function mudarVisual(cor, imagem) {
 tenis.classList.add("troca-efeito");
-
 body.style.backgroundColor = cor;
-
 botao.style.background = cor;
 
 setTimeout(() => {
     tenis.src = imagem;
     tenis.classList.remove("troca-efeito")
 }, 500);
-
-
-
-
 }
